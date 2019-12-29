25a858612634836720882a86ab9c5dcb5ca75176
jetwhisperer <Foust.Daniel.C@gmail.com>
2019-12-29 01:11:00 -0500

    Removed global variable pass-ins. Adjusted sleep times. Added "Remember me for 1 day" support and improved forced logout handling to turn 11 hr login window into 24 or 33 hr window

--- gpa_main.py
+++ gpa_main.py
@@ -11,11 +11,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 if os.path.exists('creds.yaml'):
     import yaml
+
     with open("creds.yaml", "r") as ymlfile:
         cfg = yaml.safe_load(stream=ymlfile)
 else:
     cfg = {}
 
+grades_list = {}
 
 if 'Testing' in cfg:
     testing = cfg['Testing']
@@ -40,7 +42,7 @@ if 'netID' not in cfg or not cfg['netID']:
     Or just put your netID and it will just ask for the password.
     Example:
     netID: abc123
-    password: yopasswordhere
+    password: password
     
     but for now,''')
     netID = input('netID?\n')
@@ -53,18 +55,26 @@ else:
     password = cfg['password']
 
 if testing or 'APP_KEY' not in cfg or 'APP_SECRET' not in cfg or not cfg['APP_KEY'] or not cfg['APP_SECRET']:
-# If it's testing or if they're not there or blank.
+    # If it's testing or if they're not there or blank.
     print('Add APP_KEY and APP_SECRET to creds.yaml if you want to use Pushed for notifications')
+
 
     class TerminalPush:
 
         def push_app(self, st):
             print('Simulated:', st)
 
+
     p = TerminalPush()
 else:
     import pushed
+
     p = pushed.Pushed(cfg['APP_KEY'], cfg['APP_SECRET'])
+
+
+def wait_page_change(browser):
+    current_url = browser.current_url
+    WebDriverWait(browser, 600).until(EC.url_changes(current_url))
 
 
 def browser_init():
@@ -72,11 +82,11 @@ def browser_init():
     if not testing:
         options.add_argument('-headless')
 
-    browser = webdriver.Firefox(executable_path=os.getcwd()+'/geckodriver', options=options)
+    browser = webdriver.Firefox(executable_path=os.getcwd() + '/geckodriver', options=options)
     return browser
 
 
-def browser_login(browser, netID, password):
+def browser_login(browser):
     browser.get('http://albert.nyu.edu')
     print('Browser started and at Albert...')
     browser.find_element_by_link_text('Sign in to Albert').click()  # Clicks the login thing
@@ -86,27 +96,26 @@ def browser_login(browser, netID, password):
 
     browser.find_element_by_name('_eventId_proceed').click()
     print('Browser logged in...')
+    time.sleep(3)
 
-    size = browser.get_window_size()
-    browser.set_window_size(500, 500)
-    time.sleep(2)
-    action = ActionChains(browser)
-    action.move_by_offset(200, 300)
-    action.click()
-    action.perform()
-    time.sleep(1)
+    if 'shibboleth' in browser.current_url:
+        size = browser.get_window_size()
+        browser.set_window_size(500, 510)
+        time.sleep(0.5)
 
-    print('Requesting MFA...')
-    browser.set_window_size(size['width'], size['height'])
-    current_url = browser.current_url
-    WebDriverWait(browser, 600).until(EC.url_changes(current_url))
+        action = ActionChains(browser)  # Security features don't allow selenium to click things with precision
+        action.move_by_offset(200, 430).click()  # Remember me for 1 day
+        action.pause(0.1)
+        action.move_by_offset(0, -130).click().perform()  # Send Me a Push
+        print('Requesting MFA...')
 
-    print('MFA successful...')
-    WebDriverWait(browser, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#IS_ED_SSS_GRADESLnk > span")))
+        wait_page_change(browser)
+        browser.set_window_size(size['width'], size['height'])
+        print('MFA successful...')
 
 
-def UpdateGPA(browser, p, GPAref):
-    a = browser.find_elements_by_xpath('//h2')
+def UpdateGPA(browser, GPAref):
+    a = browser.find_elements_by_xpath('//h2')  # This is ugly but there's no name for it and the xpath index varied...
     if not a:
         browser.back()
         browser.refresh()
@@ -121,17 +130,25 @@ def UpdateGPA(browser, p, GPAref):
     return GPAref
 
 
-def UpdateGrades(browser, p, grades_list):
+def UpdateGrades(browser):
     try:
-        browser.get('https://sis.portal.nyu.edu/psp/ihprod/EMPLOYEE/EMPL/h/?tab=IS_SSS_TAB&jsconfig=IS_ED_SSS_GRADESLnk')
-        WebDriverWait(browser, 60).until(
+        browser.get(
+            'https://sis.portal.nyu.edu/psp/ihprod/EMPLOYEE/EMPL/h/?tab=IS_SSS_TAB&jsconfig=IS_ED_SSS_GRADESLnk')
+        time.sleep(1)
+
+        if 'shibboleth' in browser.current_url:
+            browser_login(browser)
+            browser.get(
+                'https://sis.portal.nyu.edu/psp/ihprod/EMPLOYEE/EMPL/h/?tab=IS_SSS_TAB&jsconfig=IS_ED_SSS_GRADESLnk')
+
+        WebDriverWait(browser, 15).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "#IS_ED_SSS_GRADESLnk > span")))
         time.sleep(3)
         c = browser.find_elements_by_xpath('//div[4]/div[2]/table/tbody/tr/td')
         print('\n\n\n')
         grades = []
         for i in range(1, len(c), 6):
-            if c[i].text != 'EXAMINATION HOUR':
+            if not re.search(r'EXAMINATION HOUR', c[i].text):
                 grades.append(c[i].text + ' ' * (31 - len(c[i].text)) + ' ' + c[i + 4].text)
                 if c[i].text not in grades_list and c[i + 4].text != ' ':
                     grades_list[c[i].text] = c[i + 4].text
@@ -139,39 +156,32 @@ def UpdateGrades(browser, p, grades_list):
         grades = '\n'.join(grades)
         print(grades)
         print('Last refreshed:', time.strftime('%d%b at %H:%M'))
-        return False  # Did it crash?
+
     except Exception as e:
         print('------------------------------------------------------------------------\n'
               'There was an error. Here\'s the exception\n', str(e))
         with open('ErrorLog.txt', 'a') as file:
             print(time.strftime('%d%b at %H:%M:%S'), '\n', str(e), '\n--------\n', file=file)
-        p.push_app('App crashed')
-        return True  # Did it crash?
 
 
-def run(netID, p, password):
+def run():
     browser = browser_init()
-    browser_login(browser, netID, password)
-    del netID, password
-    grades_list = {}
+    browser_login(browser)
     if not testing:
         sleeptime = 600  # seconds
     else:
-        sleeptime = 10
+        sleeptime = 30
 
-    UpdateGrades(browser, p, grades_list)
-    GPAref = UpdateGPA(browser, p, '0')
+    UpdateGrades(browser)
+    GPAref = UpdateGPA(browser, '0')
 
     time.sleep(sleeptime)
 
-    crashed = False
-    while not crashed:
-        crashed = UpdateGrades(browser, p, grades_list)
-        GPAref = UpdateGPA(browser, p, GPAref)
+    while True:
+        UpdateGrades(browser)
+        GPAref = UpdateGPA(browser, GPAref)
 
         time.sleep(sleeptime)
 
-    return browser, p
 
-
-browser, p = run(netID, p, password)  # For debugging.
+run()
